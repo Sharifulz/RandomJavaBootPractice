17e9a8ed5231146bcef3760a90ec84e8fd5cbaa1
shaarifulz <shaarifulz@gmail.com>
2020-05-27 13:42:08 +0600

    Initializing spring boot app for pagination

--- APIFeatureTemplate/src/main/java/com/example/demo/ApiFeatureTemplateApplication.java
+++ APIFeatureTemplate/src/main/java/com/example/demo/ApiFeatureTemplateApplication.java
@@ -2,10 +2,13 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+/*
+ * @Author Shariful Islam
+ */
 
 @SpringBootApplication
 public class ApiFeatureTemplateApplication {
-
+//------------- Application start from here
 	public static void main(String[] args) {
 		SpringApplication.run(ApiFeatureTemplateApplication.class, args);
 	}
