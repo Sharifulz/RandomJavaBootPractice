02966b9774205e79c2351ff2182d3dd7396318e8
shaarifulz <shaarifulz@gmail.com>
2020-06-12 05:29:54 +0600

    For remode from a java list use stream api and filter then collect to saparate the data

--- CreateUserInMultipleDatabase/src/com/exam/collection/ListCollection.java
+++ CreateUserInMultipleDatabase/src/com/exam/collection/ListCollection.java
@@ -2,26 +2,41 @@ package com.exam.collection;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ListCollection {
 	public static void main(String[] args) {
 		List<String> list1 = new ArrayList<>();
-		List<String> list2 = new ArrayList<>();
-		list1.add("Hellow");
-		list1.add("Shariful");
-		
-		list2.add("World");
-		list2.add("Islam");
+		list1.add("A");
+		list1.add("B");
+		list1.add("B");
+		list1.add("B");
+		list1.add("C");
+		int size = list1.size();
 		/*
-		for (String list : list1) {
-			System.out.println("Value: "+ list);
+		for (int i = 0; i < list1.size(); i++) {
+			if (list1.get(i).equals("B")) {
+				list1.remove(i);
+				size--;
+			}
 		}
 		*/
-		if (list2.get(0).equals("World")) {
-			list2.remove(0);
+		/*
+		for (String string : list1) {
+			if (string.equals("B")) {
+				list1.remove(string);
+			}
 		}
-		for (String list : list2) {
-			System.out.println("Value: "+ list);
+		*/
+		
+		List<String> resultList = list1.stream().filter(str->!str.equals("B")).collect(Collectors.toList());
+		
+//		 Numbers.removeIf(n -> (n % 3 == 0));
+		 
+		 
+		for (int i = 0; i < resultList.size(); i++) {
+			System.out.println(resultList.get(i));
 		}
+		
 	}
 }
