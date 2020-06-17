62abce6f62c184638fcd78db7b7d660b9b98f584
shaarifulz <shaarifulz@gmail.com>
2020-06-17 13:10:16 +0600

    file upload and show in html page Spring Boot Rest Api call

---
+++ ImageUploadAndShow/src/main/java/com/exam/controller/FileUploadController.java
@@ -0,0 +1,48 @@
+package com.exam.controller;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/file")
+public class FileUploadController {
+private static String UPLOAD_DIR ="uploads";
+	
+	@RequestMapping(value = "/upload" , method = RequestMethod.POST)
+	public String upload(@RequestParam("file") MultipartFile file, HttpServletRequest request) {
+		try {
+			String filename = "name1.jpg";
+			String path = request.getServletContext().getRealPath("")+UPLOAD_DIR+ File.separator+filename;
+			saveFile(file.getInputStream(), path);
+			return filename;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return e.getMessage();
+		}
+	}
+	
+	public void saveFile(InputStream inputStream, String path) {
+		try {
+			OutputStream outputStream = new FileOutputStream(new File(path));
+			int read = 0;
+			byte[] bytes = new byte[1024];
+			while((read = inputStream.read(bytes)) != -1) {
+				outputStream.write(bytes, 0, read);
+			}
+			outputStream.flush();
+			outputStream.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
