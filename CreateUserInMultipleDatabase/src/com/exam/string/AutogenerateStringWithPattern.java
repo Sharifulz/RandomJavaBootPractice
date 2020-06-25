a8d985643d2d76279fa49769a212f877a9518f85
shaarifulz <shaarifulz@gmail.com>
2020-06-25 15:53:07 +0600

    Substring used

--- CreateUserInMultipleDatabase/src/com/exam/string/AutogenerateStringWithPattern.java
+++ CreateUserInMultipleDatabase/src/com/exam/string/AutogenerateStringWithPattern.java
@@ -1,5 +1,9 @@
 package com.exam.string;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.exam.oop.Project;
 
 public class AutogenerateStringWithPattern {
 	public static void main(String[] args) {
@@ -13,11 +17,38 @@ public class AutogenerateStringWithPattern {
 		
 		String fileName = "eq.jpg";
 		String[] splitArr = fileName.split("\\.");
-		System.out.println(splitArr.length);
+		//System.out.println(splitArr.length);
 		
 		for (String string : splitArr) {
-			System.out.println(string);
+			//System.out.println(string);
 		}
+		
+		String unions="";
+		
+		Project p1 = new Project("BDA", "BDAWIFI1", "Bangladesh Army Wifi Project");
+		Project p2 = new Project("BDA", "BDAWIFI2", "Bangladesh Army Wifi Project");
+		Project p3 = new Project("CTS", "BDAWIFI3", "CTrends Software Limited");
+		Project p4 = new Project("BDA", "SURVEY4", "Bangladesh Army Survey Project");
+		Project p5 = new Project("BDA", "BDAWIFI5", "Bangladesh Army Wifi Project");
+		
+		List<Project> projectList = new ArrayList<Project>();
+		projectList.add(p1);
+		projectList.add(p2);
+		projectList.add(p3);
+		projectList.add(p4);
+		projectList.add(p5);
+		
+		for (Project project : projectList) {
+			unions +=project.getProjectCode() +", ";
+		}
+		
+		String msg = unions.substring(0, unions.length()-2);
+		System.out.println(unions.length());
+		System.out.println(unions);
+		//System.out.println(msg);
+		
+		
+		
 		
 	}  
 }
