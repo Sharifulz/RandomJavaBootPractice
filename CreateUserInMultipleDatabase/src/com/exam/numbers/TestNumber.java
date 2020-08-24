e66d919f49ac91822f9b2e0f9845970b07e3a5f7
shaarifulz <shaarifulz@gmail.com>
2020-08-24 17:15:38 +0600

    Java Message digest used for hashing string

--- CreateUserInMultipleDatabase/src/com/exam/numbers/TestNumber.java
+++ CreateUserInMultipleDatabase/src/com/exam/numbers/TestNumber.java
@@ -4,9 +4,11 @@ public class TestNumber {
 
 	public static void main(String[] args) {
 		
+		byte byt = -128;
+		System.out.println(byt);
 		int a = 20;
 		int b = 100;
-		
+		int x;
 		double avg= (Double.valueOf(a)/Double.valueOf(b))*100;
 		double numb1 = 0.0;
 		double numb2;
@@ -14,6 +16,8 @@ public class TestNumber {
 		numb1 = 0.9;
 		a = 30;
 		System.out.println(a);
+		
+		System.out.println(x = 1);
 
 	}
 
