48a9bdae4c9a05bdf91372ebb995dc02cc124fb5
shaarifulz <shaarifulz@gmail.com>
2020-09-09 18:46:44 +0600

    Exception practiced

--- CreateUserInMultipleDatabase/src/com/exam/exception/ThrowsException.java
+++ CreateUserInMultipleDatabase/src/com/exam/exception/ThrowsException.java
@@ -2,26 +2,31 @@ package com.exam.exception;
 
 public class ThrowsException {
 
+	/*
+	 * java.lang.* ----> Object ----> Throwable ----- (a) Exception (a1) Checked Exception, (a2) Unchecked Exception , (b)Errors
+	 * 
+	 * Checked exceptions == Compile Time Exception Example: FileNotFoundException, IOException
+	 * 
+	 * Unchecked exceptions == Runtime Exceptions Example: ArrayIndexOutOfBoundsException, NullPointerException
+	 * 
+	 * Errors = StackOverflow, OutOfMemory
+	 * 
+	 */
 	public static void main(String[] args) {
+
+			method1("B", "B");
 		
-		try {
-			method1(null, "B");
-		} catch (Exception e) {
-			System.out.println("Inside main -----------------------------");
-			e.printStackTrace();
-		}
 		System.out.println("Outiside main ------------------- ");
 	}
 	
 	public static void method1(String x, String b){
-		try {
+	
 			if (x.equals("A")) {
 				System.out.println("Method1 "+ x);
-			}	
-		} catch (Exception e) {
-			System.out.println("Exception method 1");
-			e.printStackTrace();
-		}
+			}	else {
+				System.out.println("Else ----------------------");
+				throw new NullPointerException();
+			}
 		
 		System.out.println("Outside print statement");
 	}
---
+++ CreateUserInMultipleDatabase/src/com/exam/threads/SingleThread.java
@@ -0,0 +1,5 @@
+package com.exam.threads;
+
+public class SingleThread {
+
+}
